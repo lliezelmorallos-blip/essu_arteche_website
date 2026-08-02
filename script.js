4b88692d57070116eb2edb00877278e829898522
liezel Morallos <lliezelmorallos@gmail.com>
2026-08-03 00:04:35 +0800

    add script.js and alert

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded", function () {    const form = document.querySelector("form");    form.addEventListener("submit", function (event) {        event.preventDefault();        alert("Thank you! Your message has been submitted successfully.");        form.reset();    });});
